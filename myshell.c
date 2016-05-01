3276bc609daca6e0fb273ba3784dea9e70f692cd
hjbyt <sopu00@gmail.com>
2016-05-01 03:24:51 +0300

    ...

--- myshell.c
+++ myshell.c
@@ -4,6 +4,11 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <sys/wait.h>
+#include <sys/types.h>
 
 typedef int bool;
 #define TRUE 1
@@ -41,5 +46,29 @@ int run_piped(char** arglist1, int count1, char** arglist2, int count2)
 
 int run(char** arglist, int count, bool run_in_background)
 {
+	pid_t pid = fork();
+	if (pid == -1) {
+		perror("fork failed");
+		return 0;
+	} else if (pid == 0) {
+		// This is the child process
+		execvp(arglist[0], arglist);
+		// Note: if execvp returned, it surely failed and returned -1,
+		// hence no need to check it's return value.
+		perror("executing command failed");
+		return 0;
+	}
+	assert(pid != 0);
+	// This is the parent process.
+
+	if (!run_in_background) {
+		if (waitpid(pid, NULL, 0) == -1) {
+			perror("waiting on child failed");
+			return 0;
+		}
+	} else {
+		//TODO: ???
+	}
+
 	return 1;
 }
